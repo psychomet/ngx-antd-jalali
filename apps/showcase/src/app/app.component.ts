49e50c33ee59c432efbe63bee004873f3b98b1d0
Mahdi Sanatkar <psychomet@Mahdis-MacBook-Pro.local>
2023-12-22 15:31:30 +0330

    feat(showcase): impl ngx-antd-jalali date-picker

--- apps/showcase/src/app/app.component.ts
+++ apps/showcase/src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { NzDatePickerModule } from 'ngx-antd-jalali/date-picker';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NzDatePickerModule, FormsModule],
   selector: 'ngx-antd-jalali-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
 })
 export class AppComponent {
-  title = 'showcase';
+  date = new Date();
+
+  onChange(result: Date): void {
+    console.log('onChange: ', result);
+  }
 }
--- apps/showcase/src/app/app.config.ts
+++ apps/showcase/src/app/app.config.ts
@@ -1,7 +1,25 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { NzDateAdapter } from 'ngx-antd-jalali/core';
+import { NZ_DATE_CONFIG } from 'ngx-antd-jalali/i18n';
+import { JalaliMomentDateAdapter } from './nz-jalali-moment.adapter';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideRouter(appRoutes),
+    provideAnimations(),
+    { provide: NzDateAdapter, useClass: JalaliMomentDateAdapter },
+    {
+      provide: NZ_DATE_CONFIG,
+      useValue: {
+        displayFormats: {
+          veryShortWeekLabel: 'dd',
+          dateInput: 'yyyy/MM/DD',
+          dateTimeInput: 'yyyy-MM-DD HH:mm:ss',
+        },
+      },
+    },
+  ],
 };
